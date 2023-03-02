f7e192317a537304f26a85a4a854223c02417c1f
SvetlanaBobiles <122575469+SvetlanaBobiles@users.noreply.github.com>
2023-03-02 14:21:21 -0800

    bash script
    code

--- grade.sh
+++ grade.sh
@@ -3,3 +3,26 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [[ -f student-submission/ListExamples.java ]]
+then 
+    echo 'found correct file'
+else 
+    exit
+fi
+
+cp TestListExamples.java student-submission
+cp Server.java student-submission
+cp -r lib student-submission
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+
+if [[ $? == 0 ]]
+then
+    echo 'was able to compile'
+else
+    echo 'compile error'
+    exit
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
