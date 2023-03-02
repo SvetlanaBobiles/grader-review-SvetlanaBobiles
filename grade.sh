56d6b3a0c14faf7721c305e690144780842e5a37
SvetlanaBobiles <122575469+SvetlanaBobiles@users.noreply.github.com>
2023-03-02 14:31:03 -0800

    directory
    change

--- grade.sh
+++ grade.sh
@@ -15,6 +15,8 @@ cp TestListExamples.java student-submission
 cp Server.java student-submission
 cp -r lib student-submission
 
+cd student-submission
+
 javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
 
 if [[ $? == 0 ]]
